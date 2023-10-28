32da2824647cfd3d49317b472c4ea4a88ed2b1be
BeizzTech <piyu1818@gmail.com>
2023-10-28 20:55:19 +0530

    New 8.55pm commit

--- webTest/HelloTest.java
+++ webTest/HelloTest.java
@@ -4,6 +4,7 @@ public class HelloTest {
 
 	public static void main(String[] args)
 	{
+		System.out.println("This is simple Loop Demo");
 		for(int i=1;i<=10;i++)
 		{
 			System.out.println("Hello");
